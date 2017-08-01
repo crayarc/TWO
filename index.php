319bbd9ebb313c0ba002b91f594a48ea5a716357
Mark Aguilar <aguilarmark19@gmail.com>
2017-07-31 19:10:42 -0500

    update index

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
 
+	<body>
 <?php
 $page_title = 'Welcome to this Site!';
 include ('htdocs/includes/header.html');
 ?>
 
-hi
+<h1>About</h1>
+<p>My Name is Mark Aguilar and I am a junior at the University of Houston and am majoring in Computer Information Systems.
+	I received a formal education which has given me the experience I need to prepare for this class.
+	I went to Cypress Ridge High School and received my diploma.
+	After high school, I attended Lone Star College for two years and received an Associate’s of Science Degree.
+	I have job experience in retail and guest services.
+	Through my working experience I have developed skills such as trying to help disgruntled customers, critical thinking, and dealing with multiple problems and people simultaneously.
+	Dealing with a bulk of people on any given day has taught me to stay calm and analyze the situation at hand.
+ </p>
 
 <?php
 	include ('htdocs/includes/footer.html');
 	?>
+</html>
